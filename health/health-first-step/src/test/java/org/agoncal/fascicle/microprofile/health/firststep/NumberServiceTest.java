00e4500f29ef949b763b2eee22e458856f771ea2
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-09 11:10:46 +0200

    Health

---
+++ health/health-first-step/src/test/java/org/agoncal/fascicle/microprofile/health/firststep/NumberServiceTest.java
@@ -0,0 +1,30 @@
+package org.agoncal.fascicle.microprofile.health.firststep;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.agoncal.fascicle.microprofile.health.firststep.NumberService;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@formatter:off
+@QuarkusTest
+public class NumberServiceTest {
+
+  @Inject
+  NumberService numberService;
+
+  @Test
+  void shouldGenerateBookNumber() {
+    String number = numberService.generateBookNumber();
+    assertTrue(number.startsWith("13"));
+    assertTrue(number.endsWith("pt"));
+  }
+
+  @Test
+  void shouldGenerateCDNumber() {
+    String number = numberService.generateCDNumber();
+    assertTrue(number.endsWith("pt"));
+  }
+}
