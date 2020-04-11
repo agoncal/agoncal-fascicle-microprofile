94eb021bf1bbf6f1f5b7cedad159f7c4bc327662
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-11 11:27:50 +0200

    public accessor

--- cdi/cdi-first-step/src/main/java/org/agoncal/fascicle/microprofile/cdi/firststep/BookService.java
+++ cdi/cdi-first-step/src/main/java/org/agoncal/fascicle/microprofile/cdi/firststep/BookService.java
@@ -13,7 +13,7 @@ import javax.inject.Inject;
 public class BookService {
 
   @Inject
-  private IsbnGenerator numberGenerator;
+  IsbnGenerator numberGenerator;
 
   public Book createBook(String title, Float price, String description) {
     Book book = new Book(title, price, description);
