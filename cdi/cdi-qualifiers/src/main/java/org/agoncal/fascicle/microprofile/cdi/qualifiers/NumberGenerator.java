c14d9c84f13299ad62060995200c014c773e56a8
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-11 15:28:51 +0200

    cdi qualifiers

---
+++ cdi/cdi-qualifiers/src/main/java/org/agoncal/fascicle/microprofile/cdi/qualifiers/NumberGenerator.java
@@ -0,0 +1,16 @@
+package org.agoncal.fascicle.microprofile.cdi.qualifiers;
+
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public interface NumberGenerator {
+
+  // ======================================
+  // =          Business methods          =
+  // ======================================
+
+  String generateNumber();
+}
