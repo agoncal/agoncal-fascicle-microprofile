e3af1d8b3bd6ff98f64079ae02707d806dbb6a05
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-12 14:26:51 +0200

    JAX-RS

--- jaxrs/jaxrs-exposing/src/main/java/org/agoncal/fascicle/microprofile/jaxrs/exposing/Customers.java
+++ jaxrs/jaxrs-exposing/src/main/java/org/agoncal/fascicle/microprofile/jaxrs/exposing/Customers.java
@@ -1,8 +1,5 @@
 package org.agoncal.fascicle.microprofile.jaxrs.exposing;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
 import java.util.ArrayList;
 import java.util.List;
 
