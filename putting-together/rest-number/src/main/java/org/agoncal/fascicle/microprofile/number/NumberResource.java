22357f58302644cb38bf46281cfc084b306c9ec6
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-04-22 22:06:10 +0200

    Adoc

--- putting-together/rest-number/src/main/java/org/agoncal/fascicle/microprofile/number/NumberResource.java
+++ putting-together/rest-number/src/main/java/org/agoncal/fascicle/microprofile/number/NumberResource.java
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,6 +24,7 @@ public class NumberResource {
   private final static Logger LOGGER = Logger.getLogger(NumberResource.class.getName());
 
   // tag::adocConfig[]
+  @Inject
   @ConfigProperty(name = "number.separator", defaultValue = "false")
   boolean separator;
 
