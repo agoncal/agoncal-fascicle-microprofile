9f43da5a63b899699f16a8554c3157669a61a5a0
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-05-09 21:02:52 +0200

    OpenAPI

--- open-api/open-api-filter/src/main/java/org/agoncal/fascicle/microprofile/openapi/filter/BookResource.java
+++ open-api/open-api-filter/src/main/java/org/agoncal/fascicle/microprofile/openapi/filter/BookResource.java
@@ -3,12 +3,10 @@ package org.agoncal.fascicle.microprofile.openapi.filter;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -18,11 +16,11 @@ import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 @Path("/api/books")
 public class BookResource {
 
-  @Operation(summary = "Returns a book for a given identifier")
+  @Operation(summary = "Returns a random book")
   @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Book.class)))
   @GET
   @Path("/{id}")
-  public Response getBook(@Parameter(description = "Book identifier", required = true) @PathParam("id") Long id) {
+  public Response getRandomBook() {
     // ...
     // tag::adocSkip[]
     return Response.status(NOT_FOUND).build();
