b524a86adc38318bbf5627dca5ab5bf049131f58
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-06-23 16:34:05 +0200

    Adoc

--- config/config-injection-programmatic/src/test/java/org/agoncal/fascicle/microprofile/config/injection/InvoiceTest.java
+++ config/config-injection-programmatic/src/test/java/org/agoncal/fascicle/microprofile/config/injection/InvoiceTest.java
@@ -1,7 +1,6 @@
 package org.agoncal.fascicle.microprofile.config.injection;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.agoncal.fascicle.microprofile.config.injection.Invoice;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
@@ -9,8 +8,8 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@formatter:off
 @QuarkusTest
@@ -19,19 +18,19 @@ class InvoiceTest {
   @Inject
   Invoice invoice;
 
-  // tag::shouldCalculateInvoiceProgrammaticallyInjection[]
+  // tag::adocShouldCalculateInvoiceProgrammaticallyInjection[]
   @Inject
   Config config;
 
-  // end::shouldCalculateInvoiceProgrammaticallyInjection[]
+  // end::adocShouldCalculateInvoiceProgrammaticallyInjection[]
   @Test
-  public void shouldCalculateInvoiceProgrammaticallyInjection() {
-  // tag::shouldCalculateInvoiceProgrammaticallyInjection[]
+  public void adocShouldCalculateInvoiceProgrammaticallyInjection() {
+  // tag::adocShouldCalculateInvoiceProgrammaticallyInjection[]
   invoice.vatRate = config.getValue("invoice.vatRate", Float.class);
   invoice.allowsDiscount = config.getValue("invoice.allowsDiscount", Boolean.class);
   invoice.terms = config.getValue("invoice.terms", String.class);
   invoice.penalties = config.getValue("invoice.penalties", String.class);
-  // end::shouldCalculateInvoiceProgrammaticallyInjection[]
+  // end::adocShouldCalculateInvoiceProgrammaticallyInjection[]
 
     invoice.subtotal = 500f;
     invoice.vatAmount = invoice.subtotal * (invoice.vatRate / 100);
